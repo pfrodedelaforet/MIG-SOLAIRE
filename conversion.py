91428180236957e3f765ae5d1e3a19c4ea228fc1
JulietteGerbaux <juliette.gerbaux@mines-paristech.fr>
2019-11-20 14:44:28 +0100

    Fonction pour Tristan pour representer lat/long en m à partir dun point dorigine

---
+++ conversion.py
@@ -0,0 +1,9 @@
+from pyproj import Transformer
+transformer_to_lamb= Transformer.from_crs("EPSG:4326", "EPSG:2154", always_xy=True)
+
+x_0,y_0=transformer_to_lamb.transform(7.26528, 43.69422 )
+x_fin,y_fin=transformer_to_lamb.transform(7.28018,43.70168)
+
+def conversion(lat,long):
+    x_lamb,y_lamb=transformer_to_lamb.transform(lat,long)
+    return (x_lamb-x_0,y_lamb-y_0)
