884b0034e13096ff1ad2eed087ec9440609e66a7
Jeremy Costanzo <jeremy.costanzo@mines-paristech.fr>
2019-11-20 16:36:40 +0100

    Ajout de la classe point de Pierre

--- classes.py
+++ classes.py
@@ -5,3 +5,15 @@ class Triporteur:
         self.position = position #point
     def __repr__(self):
         str(self.capacity,self.dispo,self.position)
+        
+class Point :
+    def __init__(self, lat, lon) :
+        self.latitude = lat
+        self.longitude = lon
+        self.id = router.findNode(lat, lon)
+        self.x = transformer_to_lamb.transform(lat,lon)[0]
+        self.y = transformer_to_lamb.transform(lat,lon)[1]
+        self.alti = cartalt[round((self.x-xo) / pas)][round((self.y-yo) / pas)]
+    
+    def __repr__(self) :
+        return (f"point de latitude {self.latitude}, de longitude {self.longitude}, d'altitude {self.alti}, x={self.x}, y={self.y} ")
