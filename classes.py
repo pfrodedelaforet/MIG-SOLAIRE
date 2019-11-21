cc93a06ce64350f98d3d4dd37179351545e6513b
pfrodedelaforet <pfrodedelaforet@users.noreply.github.com>
2019-11-21 11:01:35 +0100

    modifs triporteur et delivery

--- classes.py
+++ classes.py
@@ -1,11 +1,4 @@
-class Triporteur:
-    def __init__(self,capacity,dispo,position):
-        self.capacity = capacity #flottant : poids qu'il peut porter
-        self.dispo = dispo #booleen : le triporteur est prêt à partir
-        self.position = position #point
-    def __repr__(self):
-        str(self.capacity,self.dispo,self.position)
-        
+
 class Point :
     def __init__(self, lat, lon) :
         self.latitude = lat
@@ -17,3 +10,19 @@ class Point :
     
     def __repr__(self) :
         return (f"point de latitude {self.latitude}, de longitude {self.longitude}, d'altitude {self.alti}, x={self.x}, y={self.y} ")
+class DeliveryPoint(Point):
+    def __init__(self, lat, lon, t1, t2):
+        Point.__init__(self, lat, lon)
+        self.t1 = t1
+        self.t2 = t2
+    def __repr__(self):
+        return (Point.__repr__(self)+f"il veut Ãªtre livrÃ© entre {self.t1} et {self.t2}")
+class Triporteur:
+    def __init__(self, capacity, dispo, point):
+        self.capacity = capacity #flottant : poids qu'il peut porter
+        self.dispo = dispo #booleen : le triporteur est prï¿½t ï¿½ partir
+        self.point = point #point
+    def __repr__(self):
+        str(self.capacity,self.dispo,self.position)
+           
+ 
