4645c3e32bc3bc2e0a984ed0a9351b58f8e69170
Jeremy Costanzo <jeremy.costanzo@mines-paristech.fr>
2019-11-22 17:36:33 +0100

    Correction

--- creer_liste_clients.py
+++ creer_liste_clients.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
 import numpy.random as rd
+from classes import *
+
 poids_min = 0.5 #je suppose que les colis vont de 0.5 a 3 kilogrammes
 poids_max = 3
 
-def chang_int(r):
+def change_int(r):
     return(r*(poids_max-poids_min) + poids_min)
 
 ci = np.vectorize(change_int)
@@ -16,9 +18,9 @@ tdp = np.vectorize(to_delivery_point)
         
 def creer_clients_csv(nb_clients,csv="shops.csv",horaire = (8*3600,18*3600)):
     clients = pd.read_csv(csv)
-    clients['poids'] = ci(random.sample(len(clients['osm_id']))) 
+    clients['poids'] = ci(rd.sample(len(clients['osm_id']))) 
     clients['horaires'] = np.full(len(clients['osm_id']),horaire[0])
-    clients['delivery_point'] = tdp(clients['lat'],clients['lon'],np.full(len(clients['osm_id']),horaire[0]),np.full(len(clients['osm_id']),horaire[1]))
+    clients['delivery_point'] = tdp(clients['lat'],clients['lon'],np.full(len(clients['osm_id']),horaire[0]),np.full(len(clients['osm_id']),horaire[1]),clients['poids'])
     dps = clients['delivery_point']
     return(rd.choice(dps,nb_clients,replace=False))
     
