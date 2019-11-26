e7896b380cfd98d45c0763aba090b4443253ca72
pfrodedelaforet <pfrodedelaforet@users.noreply.github.com>
2019-11-26 19:50:42 +0100

    modifs tabpoints etc

--- maptograph.py
+++ maptograph.py
@@ -13,10 +13,10 @@ from pyproj import Transformer
 transformer_to_lamb = Transformer.from_crs("EPSG:4326", "EPSG:2154", always_xy=True)
 transformer_to_lat_long = Transformer.from_crs( "EPSG:2154","EPSG:4326", always_xy=True)
 
-def distance_euc(L, N):
-    L_ = transformer_to_lamb.transform(L[0], L[1])
-    N_ = transformer_to_lamb.transform(N[0], N[1])
-    return sqrt((L_[0]-N_[0])**2+(L_[1]-N_[1])**2)
+def coor_point(coor):
+    return {Point(list(coor.keys())[i][0],list(coor.keys())[i][1]):{Point(coor[list(coor.keys())[i]][j][0], coor[list(coor.keys())[i]][j][1]) for j in range(len(coor[list(coor.keys())[i]]))} for i in range(len(list(coor.keys()))) }
+def distance_euc(point1, point2):
+    return sqrt((point1.x-point2.x)**2+(point1.y-point2.y)**2)
 
 def speed(piet, i, j):
     if piet[i][j] == 1:
@@ -24,49 +24,44 @@ def speed(piet, i, j):
     else :
         return vitesse
 
-def stop(coor):
+def stop(coor_points):
     tabstop = defaultdict(dict)
-    for i in range(len(list(coor.keys()))) : 
-        p = Point(list(coor.keys())[i][0],list(coor.keys())[i][1])
-        for j in range(len(coor[list(coor.keys())[i]])) :
-            q = Point(coor[list(coor.keys())[i]][j][0],coor[list(coor.keys())[i]][j][1]) ; tabstop[p][q] = [] 
-            for k in range(1,int(distance_euc(list(coor.keys())[i],coor[list(coor.keys())[i]][j])/10)):
+    for i in range(len(list(coor_points.keys()))) : 
+        p = list(coor_points.keys())[i]
+        for j in range(len(coor_points[list(coor_points.keys())[i]])) :
+            q = coor_points[list(coor_points.keys())[i]][j] ; tabstop[p][q] = [] 
+            for k in range(1,int(distance_euc(list(coor_points.keys())[i],coor_points[list(coor_points.keys())[i]][j])/10)):
                 if random.randrange(1, 5) == 1:
                     tabstop[p][q].append(10 *k)
     return tabstop
 
-def grosgraph(coor, altitude, velo, usager = 75, puissmax_usager = 250):
-    grosgraphe = defaultdict(dict) ; tabstop = stop(coor)
-    for i in range(len(list(coor.keys()))) : 
-        p = Point(list(coor.keys())[i][0],list(coor.keys())[i][1])
+def grosgraph(coor_points, altitude, velo, usager = 75, puissmax_usager = 250):
+    grosgraphe = defaultdict(dict) ; tabstop = stop(coor_points)
+    for i in range(len(list(coor_points.keys()))) : 
+        p = list(coor_points.keys())[i]
         grosgraphe[p] = {}
-        for j in range(len(coor[list(coor.keys())[i]])) :
-            q = Point(coor[list(coor.keys())[i]][j][0],coor[list(coor.keys())[i]][j][1])
+        for j in range(len(coor_points[list(coor_points.keys())[i]])) :
+            q = coor_points[list(coor_points.keys())[i]][j]
             if p != q: 
-                progarthur = calcul_energy([[distance_euc(list(coor.keys())[i],coor[list(coor.keys())[i]][j]), altitude[(list(coor.keys())[i][0], list(coor.keys())[i][1])], altitude[(coor[list(coor.keys())[i]][j][0], coor[list(coor.keys())[i]][j][1])], vitesse, tabstop[p][q]]], velo, usager , puissmax_usager)
+                progarthur = calcul_energy([[distance_euc(p, q), altitude[(p.latitude, p.longitude)], altitude[(p.latitude, p.longitude)], vitesse, tabstop[p][q]]], velo, usager , puissmax_usager)
                 if type(progarthur) != str:
-                    grosgraphe[p][q] = progarthur[0] + usager * distance_euc(list(coor.keys())[i],coor[list(coor.keys())[i]][j])/progarthur[2]
+                    grosgraphe[p][q] = progarthur[0] + usager * distance_euc(p,q)/progarthur[2]
                 else : 
                     grosgraphe[p][q] = float("inf")
     return (grosgraphe, tabstop)
                 
-def graphvit(coor, altitude, velo, usager = 75, puissmax_usager = 250):
+def graphvit(coor_points, altitude, velo, usager = 75, puissmax_usager = 250):
     graphvit = {}
-    for i in range(len(list(coor.keys()))) : 
-        p = Point(list(coor.keys())[i][0],list(coor.keys())[i][1])
+    for i in range(len(list(coor_points.keys()))) : 
+        p = list(coor_points.keys())[i]
         graphvit[p] = {}
-        for j in range(len(coor[list(coor.keys())[i]])) :
-            stop = []
-            for k in range(int(distance_euc(list(coor.keys())[i],coor[list(coor.keys())[i]][j])/10)):
-                if random.randrange(1, 5) == 1:
-                    stop.append(10 *k) #les 3 dernières lignes c'est la génération de la matrice stop aléatoirement
-            q = Point(coor[list(coor.keys())[i]][j][0],coor[list(coor.keys())[i]][j][1])
+        for j in range(len(coor_points[list(coor_points.keys())[i]])) :
+            stop = [] 
+            q = coor_points[list(coor_points.keys())[i]][j]
             if p!=q:
-                progarthur = calcul_energy([[distance_euc(list(coor.keys())[i],coor[list(coor.keys())[i]][j]), altitude[(list(coor.keys())[i][0], list(coor.keys())[i][1])], altitude[(coor[list(coor.keys())[i]][j][0], coor[list(coor.keys())[i]][j][1])], vitesse, stop(coor)[p][q]]], velo, usager , puissmax_usager)
+                progarthur = calcul_energy([[distance_euc(list(coor_points.keys())[i],coor_points[list(coor_points.keys())[i]][j]), altitude[(list(coor_points.keys())[i].latitude, list(coor_points.keys())[i].longitude)], altitude[(coor_points[list(coor_points.keys())[i]][j][0], coor_points[list(coor_points.keys())[i]][j][1])], vitesse, stop(coor_points)[p][q]]], velo, usager , puissmax_usager)
                 if type(progarthur) != str:
                     graphvit[p][q] = progarthur[2]
-                else :
-                    grosgraphe[p][q] = float("inf")
     return graphvit #graphvit est un graphe de point donnant la vitesse entre pointi et pointj si pointi et pointj sont adjacents
 
 
@@ -101,51 +96,51 @@ def djikstra(graphe,etape,fin,visites,dist,P,depart):
     return djikstra(graphe, x_min, fin, visites, dist, P, depart)
     
 
-def path_clients(coor, altitude, nodeslist, bornes, elp, velo, usager = 75, puissmax_usager = 250):#nodeslist et bornes sont des listes de point
+def path_clients(coor_points, altitude, nodeslist, bornes, elp, velo, usager = 75, puissmax_usager = 250):#nodeslist et bornes sont des listes de point
     M = defaultdict(dict)
-    for s in nodeslist+bornes+[elp]:
-        M[s] = {}
-        for t in nodeslist+bornes+[elp]:
-            M[s][t] = djikstra(grosgraph(coor, altitude, velo, usager = 75, puissmax_usager = 250)[0], s, t, [], graph[s], {}, s)[1]
+    for p in nodeslist+bornes+[elp]:
+        M[p] = {}
+        for q in nodeslist+bornes+[elp]:
+            M[p][q] = djikstra(grosgraph(coor_points, altitude, velo, usager, puissmax_usager)[0], p, q, [], coor_point[s], {}, p)[1]
     return M #ca renvoie un graphe de liste avec les listes de point liant les point du graphe
 
 
-def temps(coor, altitude, depart, arrivee, velo, usager = 75, puissmax_usager = 250):
-    t = 0; L = djikstra(grosgraph(coor, altitude, velo, usager = 75, puissmax_usager = 250)[0], depart, arrivee, [], graph[depart], {}, depart)[1] ; graphvitesse = graphvit(coor, velo, usager = 75, puissmax_usager = 250)
+def temps(coor_points, altitude, depart, arrivee, velo, usager = 75, puissmax_usager = 250):
+    t = 0; L = djikstra(grosgraph(coor_points, altitude, velo, usager, puissmax_usager)[0], depart, arrivee, [], coor_points[depart], {}, depart)[1] #on récup le path entre les deux points
+    graphvitesse = graphvit(coor_points, velo, usager, puissmax_usager )
     for i in range(len(L)-1):
         t += distance_euc(L[i],L[i+1])/graphvitesse[L[i]][L[i+1]]
     return t 
 
 
-def trouvpoint(coor, altitude, depart, arrivee, tdepuisdep):
-    i = 0 ; L = djikstra(grosgraph(coor, altitude, velo, usager = 75, puissmax_usager = 250)[0], depart, arrivee, [], graph[depart], {}, depart)[1]
-    while temps(coor, altitude, depart, L[i], velo, usager = 75, puissmax_usager = 250)< tdepuisdep : 
+def trouvpoint(coor_points, altitude, depart, arrivee, tdepuisdep, velo, usager = 75, puissmax_usager = 250):
+    i = 0 ; L = djikstra(grosgraph(coor_points, altitude, velo, usager, puissmax_usager)[0], depart, arrivee, [], coor_points[depart], {}, depart)[1]
+    while temps(coor_points, altitude, depart, L[i], velo, usager , puissmax_usager )< tdepuisdep : 
         i+=1
     return L[i] #c'est de la classe point
 
-def approx(nodeslist, coor):
+def approx(nodeslist, coor_points):
     for i in range(len(nodeslist)) :
-        distmin = float("inf") ; cmin = list(coor.keys())[0]
-        for c in list(coor.keys()) : 
-            if distance_euc(c, (nodeslist[i].latitude, nodeslist[i].longitude))<distmin:
-                distmin = distance_euc(c, (nodeslist[i].latitude, nodeslist[i].longitude))
+        distmin = float("inf") ; cmin = list(coor_points.keys())[0]
+        for c in list(coor_points.keys()) : 
+            if distance_euc(c, nodeslist[i])<distmin:
+                distmin = distance_euc(c, nodeslist[i])
                 cmin = c
-        nodeslist[i] = Point(cmin[0], cmin[1])
+        nodeslist[i] = cmin
     return nodeslist
 
 
-def graph(coor, altitude, nodeslist, bornes, elp, velo, usager = 75, puissmax_usager = 250):
+def graph(coor_points, altitude, nodeslist, bornes, elp, velo, usager = 75, puissmax_usager = 250):
     sousgraphe = defaultdict(dict)
     liste = nodeslist + bornes + [elp]
-    liste = approx(liste, coor) #bien une liste de points
-    grosgraphe = grosgraph(coor, altitude, velo, usager, puissmax_usager)
-    for s in liste:
-        sousgraphe[s] = {} 
-        for t in liste:
-            ener = djikstra(grosgraphe[O], s, t, [], graph[s], {}, s)[0]
-            if s != t and ener != float("inf"):
-                progarthur = calcul_energy([[distance_euc((s.latitude,s.longitude),(t.latitude, t.longitude)), altitude[(s.latitude, s.longitude)], altitude[(t.latitude, t.longitude)], vitesse, grosgraphe[1][s][t]]], velo, usager, puissmax_usager)
-                sousgraphe[s][t] = Poids(ener, temps(coor, s, t),True)
+    liste = approx(liste, coor_points) #bien une liste de points
+    grosgraphe = grosgraph(coor_points, altitude, velo, usager, puissmax_usager)
+    for p in liste:
+        sousgraphe[p] = {} 
+        for q in liste:
+            ener = djikstra(grosgraphe[0], p, q, [], coor_points[p], {}, p)[0]
+            if (p != q and ener != float("inf")):
+                sousgraphe[s][t] = Poids(ener, temps(coor_points, altitude, p, q, velo, usager, puissmax_usager),True)
                 
     return sousgraphe
 #attention les bornes et les points de livraison sont seulement des points ici, pour les différencier il faut avoir la liste des bornes                                 
